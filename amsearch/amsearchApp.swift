d504b2e428aaad27fd83c172673c27081245a9c9
Artem <114776102+amolon615@users.noreply.github.com>
2024-08-01 17:59:08 +0100

    Initial Commit

---
+++ amsearch/amsearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  amsearchApp.swift
+//  amsearch
+//
+//  Created by amolonus on 01/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct amsearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
